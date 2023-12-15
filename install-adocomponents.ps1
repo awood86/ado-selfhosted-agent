3826bf5219a400f021c93b506aa9192620632115
Andrew Wood <andrew.wood@simpson-associates.co.uk>
2023-12-15 14:52:37 +0000

    update script

--- install-adocomponents.ps1
+++ install-adocomponents.ps1
@@ -1,5 +1,5 @@
 #  Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install PWSH
 choco install powershell-core -y
