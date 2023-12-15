9311453dba145ab976131fdfd1b6286127c6ea62
Andrew Wood <andrew.wood@simpson-associates.co.uk>
2023-12-15 15:19:47 +0000

    revert to choco

--- install-adocomponents.ps1
+++ install-adocomponents.ps1
@@ -1,26 +1,17 @@
-# Disable Enhanced Internet Explorer Security Configuration
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}' -Name 'IsInstalled' -Value 0
+#  Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Install PowerShell Core
-iex "& { $(irm https://aka.ms/install-powershell.ps1) }"
+# Install PWSH
+choco install powershell-core -y
 
-# Install Az PowerShell Module
-Install-Module -Name Az -AllowClobber -Scope CurrentUser
+# Install AZ CLI
+choco install azure-cli -y
 
-# Install Az CLI
-Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+# Install git
+choco install git -y
 
-# Install Git
-$gitUrl = "https://github.com/git-for-windows/git/releases/download/v2.33.0.windows.2/Git-2.33.0.2-64-bit.exe"
-$gitFile = "$env:TEMP\gitinstall.exe"
-Invoke-WebRequest -Uri $gitUrl -OutFile $gitFile
-Start-Process -FilePath $gitFile -Args "/VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /NOICONS /COMPONENTS=\"icons,ext\reg\shellhere,assoc,assoc_sh\"" -Wait
-Remove-Item $gitFile
+# Instal AZ Powershell
+choco install az.powershell -y
 
 # Install Bicep
-$installPath = "C:\bicep\bicep.exe"
-New-Item -ItemType Directory -Force -Path "C:\bicep"
-Invoke-WebRequest -Uri https://github.com/Azure/bicep/releases/latest/download/bicep-win-x64.exe -OutFile $installPath
-# Add bicep to PATH
-$env:Path += ";C:\bicep"
-[Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
+choco install bicep -y
