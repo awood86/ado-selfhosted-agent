9751c0c65fc55a8853d62f1e3ee9687b4e7cf562
Andrew Wood <andrew.wood@simpson-associates.co.uk>
2023-12-17 23:12:42 +0000

    change to Az install

--- install-adocomponents.ps1
+++ install-adocomponents.ps1
@@ -5,7 +5,11 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 choco install powershell-core -y
 
 # Install Az Powershell Module
-choco install az.powershell -y
+Install-PackageProvider NuGet -Force
+Import-PackageProvider NuGet -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name Az -Force -AllowClobber
+Install-Module -Name Az.Accounts -Force -AllowClobber
 
 # Install Azure CLI with pwsh
 choco install azure-cli -y
